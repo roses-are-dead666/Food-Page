560dbf55370f23dc21a752cb44b77fb97eccb2c5
Arzhan Bakytbek <79097350+roses-are-dead666@users.noreply.github.com>
2021-09-11 17:07:07 +0600

    Delete scripts.js

--- scripts.js
+++
@@ -1,7 +0,0 @@
-let menu = document.querySelector('#menuBar');
-let navbar = document.querySelector('.navbar');
-
-menu.onclick = () =>{
-  menu.classList.toggle('fa-times');
-  navbar.classList.toggle('active');
-}
