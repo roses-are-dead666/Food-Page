78cd8167a63d511b949016430b369c37e18f96ba
Arzhan Bakytbek <79097350+roses-are-dead666@users.noreply.github.com>
2021-09-11 15:09:32 +0600

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,7 @@
+let menu = document.querySelector("#menuBar");
+let navbar = document.querySelector(".navbar");
+
+menu.onclick = () => {
+    menu.classList.toggle("fa-times");
+    navbar.classList.toggle("active");
+}
