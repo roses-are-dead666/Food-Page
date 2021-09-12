3ed4acc3fb35c717e38f3c204b3b7e56829588e1
Arzhan Bakytbek <79097350+roses-are-dead666@users.noreply.github.com>
2021-09-12 17:38:49 +0600

    Update scripts.js

--- JS/scripts.js
+++ JS/scripts.js
@@ -18,3 +18,17 @@ document.querySelector("#menuSearch").onclick = () => {
 document.querySelector("#close").onclick = () => {
     document.querySelector("#searchForm").classList.remove('active')
 }
+
+var swiper = new Swiper(".homeSlider", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+      delay: 7500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    loop: true,
+  });
